403f5aaef8c519cea56eab7bc5b8508fc803e7c8
ChristopherGabba <chrisgabba2005@yahoo.com>
2025-08-02 13:07:16 -0500

    fix: ios app entitlements on main app

--- src/expo-plugin/ios/withMainAppAppGroupEntitlement.ts
+++ src/expo-plugin/ios/withMainAppAppGroupEntitlement.ts
@@ -2,14 +2,24 @@ import { ConfigPlugin, withEntitlementsPlist } from '@expo/config-plugins';
 import { ConfigProps } from '../@types';
 import { getAppGroup } from '../support/iosConstants';
 
+/**
+ * Add "App Group" permission
+ * @see https://documentation.onesignal.com/docs/react-native-sdk-setup#step-4-install-for-ios-using-cocoapods-for-ios-apps (step 4.4)
+ */
 export const withMainAppAppGroupEntitlement: ConfigPlugin<ConfigProps> = (
   config
 ) => {
-  return withEntitlementsPlist(config, async (modConfig) => {
-    const appIdentifier = modConfig.ios?.bundleIdentifier!;
-    modConfig.modResults['com.apple.security.application-groups'] = [
-      getAppGroup(appIdentifier),
-    ];
-    return modConfig;
+  const APP_GROUP_KEY = 'com.apple.security.application-groups';
+  return withEntitlementsPlist(config, (newConfig) => {
+    if (!Array.isArray(newConfig.modResults[APP_GROUP_KEY])) {
+      newConfig.modResults[APP_GROUP_KEY] = [];
+    }
+    const modResultsArray = newConfig.modResults[APP_GROUP_KEY] as Array<any>;
+    const entitlement = getAppGroup(newConfig?.ios?.bundleIdentifier!);
+    if (modResultsArray.indexOf(entitlement) !== -1) {
+      return newConfig;
+    }
+    modResultsArray.push(entitlement);
+    return newConfig;
   });
 };
