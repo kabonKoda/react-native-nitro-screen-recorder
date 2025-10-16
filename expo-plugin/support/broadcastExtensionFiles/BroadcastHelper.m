432ea7bdf451d0ca46baaf1610be3ecd3d1a6ce8
kabonKoda <70214907+kabonKoda@users.noreply.github.com>
2025-10-16 03:24:21 -0700

    feat: implement streaming 5

--- expo-plugin/support/broadcastExtensionFiles/BroadcastHelper.m
+++
@@ -1,8 +0,0 @@
-#import "BroadcastHelper.h"
-
-void finishBroadcastGracefully(RPBroadcastSampleHandler * _Nonnull handler) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wnonnull"
-    [handler finishBroadcastWithError:nil];   // ← the magic line ✨
-#pragma clang diagnostic pop
-}
